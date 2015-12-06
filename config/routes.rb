3e7748900ab84c9e5aa47a6c19f1fb847e07045e
omarc7 <omarch7@gmail.com>
2015-12-07 02:56:19 +0800

    First Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts
   root 'static_pages#home'
 
