94a9f119cbd7c8dac0700ce67fe216875911a0fc
omarc7 <omarch7@gmail.com>
2015-12-08 14:22:16 +0800

    Add following users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Users
+
 User.create!(name: 'Example User',
              email: 'example@railstutorial.org',
              password: 'foobar',
@@ -24,6 +19,8 @@ User.create!(name: 'Example User',
                activated_at: Time.zone.now)
 end
 
+# Microposts
+
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
@@ -31,3 +28,12 @@ users = User.order(:created_at).take(6)
     user.microposts.create!(content: content)
   }
 end
+
+# Following relationships
+
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
